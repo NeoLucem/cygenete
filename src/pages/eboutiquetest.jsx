82088a45b55ed0601b5c7c861159de6b8b9c4214
NeoLucem <98679519+NeoLucem@users.noreply.github.com>
2023-03-11 09:25:41 +0000

    add some features on e-boutique-test

--- src/pages/eboutiquetest.jsx
+++ src/pages/eboutiquetest.jsx
@@ -59,6 +59,115 @@ function Eboutiquetest() {
                 <img src={profilePic} alt="proile" srcset="" className='img' />
             </div>
         </header>
+
+        <section className="section-1">
+
+            <div style={{display: 'flex', justifyContent: 'center'}}>
+                <h3 className='text-center text-light fs-6 mb-4' style={{width: '50%'}}>
+                    <span>Faites vos achats et expositions sans inquiétude</span>
+                </h3>
+            </div>
+
+            <div className="container d-flex justify-content-center mb-3">
+                <form action="">
+                    <input type="text" name="recherche" id="search" className='search-bar' placeholder='Recherche'/>
+                </form>
+            </div>
+
+            <h4 className="text-center fs-6 text-light">PARTICIER A VOS ACHATS</h4>
+
+            <div style={{display: 'flex', justifyContent: 'center'}}>
+                <hr style={{width: '50%', borderTop: '2px solid #fffff'}} />
+            </div>
+
+            <div className='container mb-4'>
+                <ul className='primary-navigation d-flex' style={{}}>
+                    <li><a href="#" className='active'><span>Mode</span></a></li>
+                    <li><a href="#"><span>Electronique</span></a></li>
+                    <li><a href="#"><span>Maison</span></a></li>
+                    <li><a href="#"><span>E-boutique</span></a></li>
+                </ul>
+            </div>
+
+            <div className="container mb-3">
+                <div className="row">
+                    <div className="col-sm-12 col-md-12 col-lg-12">
+                        <div class="card card-ads-bg" style={{height: '32vh'}}>
+                            {/* <div class="card-body" style={{border: 'none', borderRadius: 'none'}}>
+                                <p class="card-text-ads-1 fs-3 fw-bold">
+                                Lorem ipsum dolor sit amet consectetur.
+                                </p><br />
+                                <span className="text-ads-link"><a href="www.google.com">Acheter</a></span>
+                            </div> */}
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div className='container mb-3' style={{display: 'flex', justifyContent: 'center'}}>
+                <h3 className='text-center text-light fs-6 mb-4' style={{width: '50%'}}>
+                    <span>La mode est tout ce qui nous fait vivre textillement</span>
+                </h3>
+            </div>
+
+            <div className='container mb-3' style={{display: 'flex', justifyContent: 'center'}}>
+                <h2 className='text-center text-light mb-4' >
+                    <span>
+                        Produits inédits de l'année 2023
+                    </span>
+                </h2>
+            </div>
+
+            <div className="container mb-3">
+                <div className="row gy-2">
+                    <div className="col-sm-12 col-md-6 col-lg-6">
+                        <div class="card card-ads-bg" style={{height: '32vh'}}>
+                            {/* <div class="card-body" style={{border: 'none', borderRadius: 'none'}}>
+                                <p class="card-text-ads-1 fs-3 fw-bold">
+                                Lorem ipsum dolor sit amet consectetur.
+                                </p><br />
+                                <span className="text-ads-link"><a href="www.google.com">Acheter</a></span>
+                            </div> */}
+                        </div>
+                    </div>
+
+                    <div className="col-sm-12 col-md-6 col-lg-6">
+                        <div class="card card-ads-bg" style={{height: '32vh'}}>
+                            {/* <div class="card-body" style={{border: 'none', borderRadius: 'none'}}>
+                                <p class="card-text-ads-1 fs-3 fw-bold">
+                                Lorem ipsum dolor sit amet consectetur.
+                                </p><br />
+                                <span className="text-ads-link"><a href="www.google.com">Acheter</a></span>
+                            </div> */}
+                        </div>
+                    </div>
+                    
+                    <div className="col-sm-12 col-md-6 col-lg-6">
+                        <div class="card card-ads-bg" style={{height: '32vh'}}>
+                            {/* <div class="card-body" style={{border: 'none', borderRadius: 'none'}}>
+                                <p class="card-text-ads-1 fs-3 fw-bold">
+                                Lorem ipsum dolor sit amet consectetur.
+                                </p><br />
+                                <span className="text-ads-link"><a href="www.google.com">Acheter</a></span>
+                            </div> */}
+                        </div>
+                    </div>
+                    
+                    <div className="col-sm-12 col-md-6 col-lg-6">
+                        <div class="card card-ads-bg" style={{height: '32vh'}}>
+                            {/* <div class="card-body" style={{border: 'none', borderRadius: 'none'}}>
+                                <p class="card-text-ads-1 fs-3 fw-bold">
+                                Lorem ipsum dolor sit amet consectetur.
+                                </p><br />
+                                <span className="text-ads-link"><a href="www.google.com">Acheter</a></span>
+                            </div> */}
+                        </div>
+                    </div>
+                    
+                </div>
+            </div>
+
+        </section>
     </div>
   )
 }
